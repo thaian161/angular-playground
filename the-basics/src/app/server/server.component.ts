b85bb4fb7ab9fbe08c043a33f3ae77654c5dc7ec
Ann Bui <thaian161@yahoo.com>
2023-02-20 14:40:47 -0600

    ngStyle

--- the-basics/src/app/server/server.component.ts
+++ the-basics/src/app/server/server.component.ts
@@ -9,7 +9,16 @@ export class ServerComponent {
  serverId: number = 10;
  serverStatus: string = "offline";
 
+ //build in method of class component, it called once this component got created 
+ constructor(){
+  this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+ }
+
  getServerStatus() {
   return this.serverStatus;
+ }
+
+ getColor(){
+  return this.serverStatus === 'online' ? 'lightGreen' : 'pink';
  }
 }
